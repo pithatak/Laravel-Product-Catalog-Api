b59419c48c87dddcf79ed91e7b97a9e74cc6c986
illia <poseva41@gmail.com>
2025-11-30 23:54:51 +0100

    deploy preparation 20

--- render-start.sh
+++ render-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 php artisan key:generate --force
 php artisan migrate --force
 php artisan app:import-products
 
-RUN npm install
-RUN npm run build
+npm install
+npm run build
 
 php -S 0.0.0.0:$PORT -t public
